6b787f8001d10226c53c42a1335ba0337e8b7199
sophia-jihye <jihyeparkk@naver.com>
2019-10-06 13:42:06 +0900

    download only

--- config.py
+++ config.py
@@ -8,32 +8,36 @@ print('base_dir: ', base_dir)
 output_base_dir = os.path.join(base_dir, "output")
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--start_page_year', type=str)  # 1997
-parser.add_argument('--end_page_year', type=str)  # 2019
+parser.add_argument('--start_year', type=str)  # 1997
+parser.add_argument('--end_year', type=str)  # 2019
 args = parser.parse_args()
 
 pdf_dir = os.path.join(output_base_dir, "pdf")
 pkl_dir = os.path.join(output_base_dir, "pkl")
 txt_dir = os.path.join(output_base_dir, "txt")
 err_dir = os.path.join(output_base_dir, "err")
+err_web2pdf_dir = os.path.join(err_dir, "web2pdf")
+err_pdf2txt_dir = os.path.join(err_dir, "pdf2txt")
+err_txt2csv_dir = os.path.join(err_dir, "txt2csv")
 
 params = {
     'bis_sleep': bis_sleep,
     'base_dir': base_dir,
     'output_base_dir': output_base_dir,
 
-    'start_page_year': args.start_page_year,
-    'end_page_year': args.end_page_year,
+    'start_year': args.start_year,
+    'end_year': args.end_year,
 
     'pdf_dir': pdf_dir,
     'pkl_dir': pkl_dir,
     'txt_dir': txt_dir,
     'err_dir': err_dir,
+    'err_web2pdf_dir': err_web2pdf_dir,
+    'err_pdf2txt_dir': err_pdf2txt_dir,
+    'err_txt2csv_dir': err_txt2csv_dir,
 
-    'err_web2pdf_log_filepath': os.path.join(err_dir, "err_web2pdf.log"),
-    'err_pdf2txt_log_filepath': os.path.join(err_dir, "err_pdf2txt.log"),
-    'err_txt2csv_log_filepath': os.path.join(err_dir, "err_txt2csv.log"),
     'bis_wo_content_dict_pkl_filename_prefix': "bis_wo_content_dict_",
     'bis_w_content_csv_filepath': os.path.join(output_base_dir, "bis_w_content_FINAL.csv"),
-    'bis_w_content_pkl_filepath': os.path.join(output_base_dir, "bis_w_content_FINAL.pkl")
+    'bis_w_content_pkl_filepath': os.path.join(output_base_dir, "bis_w_content_FINAL.pkl"),
+    'unexpected_errlog_filepath': os.path.join(output_base_dir, datetime.now().strftime("%Y%m%d-%H-%M-%S") + ".txt")
 }
